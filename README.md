dba21312d9909c082b49f3fdacb04a92aa8080ba
Everett Summer <55684557+EverettSummer@users.noreply.github.com>
2022-04-20 07:21:44 -0700

    Add new sdk by openapi-generators

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # bgm-sdk
 Typescript SDK for bgm.tv
+
+Currently, it has the following version:
+
+## Inversify version
+
+Need to use with Inversify and RxJS 6+
---
+++ scripts/publish.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cp sdk/typescript/inversify/package.json sdk/typescript/inversify/dist/
+cp sdk/typescript/inversify/tsconfig.json sdk/typescript/inversify/dist/
